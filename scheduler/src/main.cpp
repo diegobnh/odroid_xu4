dc43681d8e881a03de0a66b88d693d68a007bb16
diegobnh <37424347+diegobnh@users.noreply.github.com>
2019-09-13 07:26:33 -0300

    add main branch predict

--- scheduler/src/main.cpp
+++ scheduler/src/main.cpp
@@ -16,17 +16,23 @@
 #define FLAG_ONLY_PARALLEL_REGION 0
 #define NUM_EPISODES 1000
 
+
 char** environ;
 
 static FILE* collect_stream = 0;
 static FILE* cpu_utilization_stream = 0;
+static int scheduler_input_pipe = -1;
+static int scheduler_output_pipe = -1;
+static int scheduler_pid = -1;
 static int application_pid = -1;
 static uint64_t application_start_time = 0;
 static State current_state;
 static int num_time_steps = 0;
 static int flag_update_schedule;
 
+
 static void update_scheduler_to_serial_region();
+
 
 void get_cpu_usage(double *cpu_usage)
 {
@@ -101,7 +107,66 @@ void get_cpu_usage(double *cpu_usage)
 
     cpu_usage[0] = total_cluster_little;
     cpu_usage[1] = total_cluster_big;
+    //fprintf(stderr, "Inside CPU utilization Little:%lf\t Big:%lf  Current_state:%d\n", cpu_usage[0], cpu_usage[1], current_state);
+}
 
+
+
+static void send_to_scheduler(const char* fmt, ...)
+{
+    char buffer[512];
+
+    va_list va;
+    va_start(va, fmt);
+    int count = vsnprintf(buffer, sizeof(buffer) - 1, fmt, va);
+    va_end(va);
+
+    if(count < 0)
+    {
+        perror("scheduler: failed to vsnprintf during send_to_scheduler");
+        abort();
+    }
+    else
+    {
+        buffer[count++] = '\n';
+        buffer[count] = '\0';
+
+        int written = write(scheduler_input_pipe, buffer, count);
+        if(written == -1)
+        {
+            perror("scheduler: failed to write to scheduler");
+            abort();
+        }
+        else if(written != count)
+        {
+            fprintf(stderr, "scheduler: count mismatch during send_to_scheduler\n");
+            abort();
+        }
+    }
+}
+
+static void recv_from_scheduler(const char* fmt, ...)
+{
+    int count = 0;
+    char buffer[512];
+
+    while(count == 0 || buffer[count-1] != '\n')
+    {
+        const auto result = read(scheduler_output_pipe, buffer, sizeof(buffer) - count);
+        if(result <= 0)
+        {
+            perror("scheduler: failed to read from scheduler pipe\n");
+            abort();
+        }
+
+        count += result;
+        assert(count < (int) sizeof(buffer) - 1);
+    }
+
+    va_list va;
+    va_start(va, fmt);
+    vsscanf(buffer, fmt, va);
+    va_end(va);
 }
 
 static void cleanup()
@@ -143,44 +208,9 @@ static void cleanup()
 
 static bool create_logging_file()
 {
-
-
-#ifdef PMCS_A15_ONLY
-    char pmcs[10][35]={"0x01_0x02_0x03_0x04_0x05_0x08",
-                       "0x09_0x10_0x12_0x13_0x14_0x15",
-                       "0x16_0x17_0x18_0x19_0x1B_0x1D",
-                       "0x40_0x41_0x42_0x43_0x46_0x47",
-                       "0x48_0x4C_0x4D_0x50_0x51_0x52",
-                       "0x53_0x56_0x58_0x60_0x61_0x62",
-                       "0x64_0x66_0x67_0x68_0x69_0x6A",
-                       "0x6C_0x6D_0x6E_0x70_0x71_0x72",
-                       "0x73_0x74_0x75_0x76_0x78_0x79",
-                       "0x7A_0x7E_0x00_0x00_0x00_0x00"};
-
-#elif defined PMCS_A7_ONLY
-    char pmcs[9][35]={"0x01_0x02_0x03_0x04",
-                      "0x05_0x06_0x07_0x08",
-                      "0x09_0x0A_0x0C_0x0D",
-                      "0x0E_0x0F_0x10_0x12",
-                      "0x13_0x14_0x15_0x16",
-                      "0x17_0x18_0x19_0x1D",
-                      "0x60_0x61_0xC0_0xC1",
-                      "0xC4_0xC5_0xC6_0xC9",
-                      "0xCA_0x00_0x00_0x00"};
-#endif
-
-    static int index_pmc=0;
     char filename[PATH_MAX];
 
-#if defined PMCS_A7_ONLY || defined PMCS_A15_ONLY 
-    if(index_pmc >=0 && index_pmc<=10){
-       sprintf(filename, "%s.csv", pmcs[index_pmc]);
-       index_pmc ++;
-    }
-#else
     sprintf(filename, "scheduler_%d.csv", application_pid); //getpid() get fathers'pid  
-#endif
-
 
     collect_stream = fopen(filename, "w");
     if(!collect_stream)
@@ -189,7 +219,7 @@ static bool create_logging_file()
         return false;
     }
     fprintf(stderr, "scheduler: collecting to file %s\n", filename);
-   
+    
     return true;
 }
 
@@ -208,6 +238,52 @@ static bool create_time_file(uint64_t time_ms)
     return true;
 }
 
+static bool spawn_agent(const char* command)
+{
+    int inpipefd[2] = {-1, -1};
+    int outpipefd[2] = {-1, -1};
+
+    if(pipe(inpipefd) == -1 || pipe(outpipefd) == -1)
+    {
+        perror("scheduler: failed to create scheduling pipes.");
+        return false;
+    }
+
+    int pid = fork();
+    if(pid == -1)
+    {
+        perror("scheduler: failed to fork scheduler");
+        close(inpipefd[0]);
+        close(inpipefd[1]);
+        close(outpipefd[0]);
+        close(outpipefd[1]);
+        return false;
+    }
+    else if(pid == 0)
+    {
+        dup2(outpipefd[0], STDIN_FILENO);
+        dup2(inpipefd[1], STDOUT_FILENO);
+
+        close(outpipefd[1]);
+        close(inpipefd[0]);
+
+        // receive SIGTERM once the parent process dies
+        prctl(PR_SET_PDEATHSIG, SIGTERM);
+
+        execl("/bin/sh", "sh", "-c", command, NULL);
+        perror("scheduler: execl failed");
+        return false;
+    }
+    else
+    {
+        close(outpipefd[0]);
+        close(inpipefd[1]);
+        ::scheduler_pid = pid;
+        ::scheduler_input_pipe = outpipefd[1];
+        ::scheduler_output_pipe = inpipefd[0];
+        return true;
+    }
+}
 
 static bool spawn_application(char* argv[])
 {
@@ -305,6 +381,7 @@ static void update_scheduler()
 
     }
 
+
     double total_cpu_migration = 0;
     double total_context_switch = 0;
 
@@ -319,42 +396,57 @@ static void update_scheduler()
     const uint64_t elapsed_time = to_millis(get_time() - ::application_start_time);
     State next_state = current_state;
 
+    int state_index_reply;
+    float exec_time = -1.0;
+
+    send_to_scheduler("%a %a %a %a %a %a %a %a %a %a %a %a %a %a %a %a %d %f", \
+                      l_pmc_1, l_pmc_2, l_pmc_3, l_pmc_4, l_pmc_5, \
+                      b_pmc_1, b_pmc_2, b_pmc_3, b_pmc_4, b_pmc_5, b_pmc_6, b_pmc_7, \
+                      total_cpu_migration, total_context_switch, cpu_usage[0], cpu_usage[1], \
+                      current_state, exec_time);
+
+    recv_from_scheduler("%d", &state_index_reply);//Here is State enumerate
+    ::num_time_steps += 1;
+    next_state = static_cast<State>(state_index_reply);
 
 
-#ifdef PMCS_A15_ONLY   
-    fprintf(collect_stream, "%" PRIu64 \
-                            ",%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf," \
-                            "%.2lf,%.2lf,%.2lf,%.2lf\n" ,
-                            elapsed_time, \
-                            b_pmc_1, b_pmc_2, b_pmc_3, b_pmc_4, b_pmc_5, b_pmc_6, b_pmc_7, \
-                            total_cpu_migration, total_context_switch, cpu_usage[0], cpu_usage[1]);
+    if(::application_pid != -1 && next_state != current_state)
+    {
+        char buffer[512];
+        auto cfg = configs[next_state];//extern variable declared in States.h
+
+        sprintf(buffer, "taskset -pac %s %d >/dev/null", cfg, application_pid);
+        fprintf(stderr, "scheduler: %s\n", buffer);
 
 
-#elif defined PMCS_A7_ONLY        
-    fprintf(collect_stream, "%" PRIu64 \
-                            ",%.2lf,%.2lf,%.2lf,%.2lf,%.2lf," \
-                            "%.2lf,%.2lf,%.2lf,%.2lf\n" ,
-                            elapsed_time, \
-                            l_pmc_1, l_pmc_2, l_pmc_3, l_pmc_4, l_pmc_5, \
-                            total_cpu_migration, total_context_switch, cpu_usage[0], cpu_usage[1]);
+        perf_shutdown();
+        if(next_state == STATE_4l)
+        {
+            perf_init_little()
+        }
+        else if (next_state == STATE_4b)
+        {
+            perf_init_big()
+        }
+        else if (next_state == STATE_4b4l)
+        {
+            perf_init_biglittle()
+        }
 
 
-#else
-    fprintf(stderr,"%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf,%.2lf," \
-                   "%.2lf,%.2lf,%.2lf,%.2lf\n", \
-                   l_pmc_1, l_pmc_2, l_pmc_3, l_pmc_4, l_pmc_5, \
-                   b_pmc_1, b_pmc_2, b_pmc_3, b_pmc_4, b_pmc_5, b_pmc_6, b_pmc_7, \
-                   total_cpu_migration, total_context_switch, cpu_usage[0], cpu_usage[1]);
+        int status = system(buffer);
+        if(status == -1)
+        {
+            perror("scheduler: system() failed");
+        }
+        else if(status != 0)
+        {
+            fprintf(stderr, "scheduler: taskset returned %d :(\n", status);
+        }
 
-    fprintf(collect_stream, "%" PRIu64 \
-                            ",%lf,%lf,%lf,%lf,%lf," \
-                            "%lf,%lf,%lf,%lf,%lf,%lf,%lf," \
-                            "%.2lf,%.2lf,%.2lf,%.2lf\n" ,
-                            elapsed_time, \
-                            l_pmc_1, l_pmc_2, l_pmc_3, l_pmc_4, l_pmc_5, \
-                            b_pmc_1, b_pmc_2, b_pmc_3, b_pmc_4, b_pmc_5, b_pmc_6, b_pmc_7, \
-                            total_cpu_migration, total_context_switch, cpu_usage[0], cpu_usage[1]);
-   
+        current_state = next_state;
+    }
+
 }
 
 
@@ -362,20 +454,10 @@ void sig_handler(int signo)
 {
     if (signo == SIGUSR1){
        fprintf(stderr, "received SIGUSR1\n");
-        if(FLAG_ONLY_PARALLEL_REGION == 1){
-             update_scheduler();
-             ::flag_update_schedule = 0;
-        }
 
     }
     else if (signo == SIGUSR2){
-       fprintf(stderr, "received SIGUSR2\n"); 
-
-       if(FLAG_ONLY_PARALLEL_REGION == 1){
-          ::flag_update_schedule = 1;
-          fprintf(collect_stream, "\n"); 
-       }
-       //update_scheduler_to_serial_region();
+       fprintf(stderr, "received SIGUSR2\n");
     }
 }
 
@@ -393,23 +475,17 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-#if defined PMCS_A15_ONLY
-    const int num_episodes = 10;//number of time to collect all pmcs from big core
-#elif defined PMCS_A7_ONLY
-    const int num_episodes = 9;//number of time to collect all pmcs from little core
-#else
-    const int num_episodes = 1;
+    const int num_episodes = 1; // Predictor should run a single episode
+    if(!spawn_agent("python3 ./predictor.py"))
+    {
+        cleanup();
+        return 1;
+    }
 
     for(int curr_episode = 0; curr_episode < num_episodes; ++curr_episode)
     {
         perf_init();
 
-        if(!create_logging_file())
-        {
-            cleanup();
-            return 1;
-        }
-        if(!spawn_application(&argv[1]))
         {
             cleanup();
             return 1;
@@ -430,21 +506,20 @@ int main(int argc, char* argv[])
                 assert(pid == ::application_pid);
                 application_pid = -1;
 
+                float exec_time;
+
+                exec_time = to_millis(get_time() - ::application_start_time);
+
             }
-            #if SCHEDULER_TYPE == SCHEDULER_TYPE_COLLECT
-            else if(!(flag_update_schedule == 1))
-            {
-                update_scheduler();
-            }
-            #endif
             usleep(200000);//20 miliseconds
         }
 
         perf_shutdown();
 
 
-        create_time_file(to_millis(get_time() - ::application_start_time));
- 
+        create_time_file(exec_time);
+
+
         usleep(5000000); //only to clear anything in cpu - 2 seconds
         fprintf(stderr, "scheduler: episode %d finished\n", curr_episode + 1);
     }
