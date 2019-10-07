530bca641e0542e4c5a7fe3d403775f338926fa1
diegobnh <37424347+diegobnh@users.noreply.github.com>
2019-10-07 19:39:12 -0300

    few changes

--- scripts/0-collect/collect_pmcs_bots.sh
+++ scripts/0-collect/collect_pmcs_bots.sh
@@ -16,35 +16,35 @@ rm -f -- *.csv
 SUIT_NAME="4l_bots_"
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/fib.gcc.omp-tasks-tied -o 0 -n 36
-mkdir $SUIT_NAME"fib"; mv *.csv $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
+mkdir $SUIT_NAME"fib"; mv *.csv *.energy  $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
 rm -r $SUIT_NAME"fib"; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/nqueens.gcc.omp-tasks-tied -n 13
-mkdir $SUIT_NAME"nqueens"; mv *.csv $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
+mkdir $SUIT_NAME"nqueens"; mv *.csv *.energy  $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
 rm -r $SUIT_NAME"nqueens"; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/health.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/health/medium.input
-mkdir $SUIT_NAME"health"; mv *.csv $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
+mkdir $SUIT_NAME"health"; mv *.csv *.energy  $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
 rm -r $SUIT_NAME"health"; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/floorplan.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/floorplan/input.20
-mkdir $SUIT_NAME"floorplan"; mv *.csv $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
+mkdir $SUIT_NAME"floorplan"; mv *.csv *.energy  $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
 rm -r $SUIT_NAME"floorplan"; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/fft.gcc.omp-tasks-tied -o 0 -n 10000000
-mkdir $SUIT_NAME"fft" ; mv *.csv $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
+mkdir $SUIT_NAME"fft" ; mv *.csv *.energy  $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
 rm -r $SUIT_NAME"fft" ; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/sort.gcc.omp-tasks-tied -o 0 -n 100000000
-mkdir $SUIT_NAME"sort"; mv *.csv $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
+mkdir $SUIT_NAME"sort"; mv *.csv *.energy  $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
 rm -r $SUIT_NAME"sort" ; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/sparselu.gcc.for-omp-tasks-tied -o 0 -n 100 -m 100
-mkdir $SUIT_NAME"sparselu" ; mv *.csv $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
+mkdir $SUIT_NAME"sparselu" ; mv *.csv *.energy  $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
 rm -r $SUIT_NAME"sparselu" ; sleep 3;
 
 taskset -a -c 0-3 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/strassen.gcc.omp-tasks-tied -o 0 -n 4096
-mkdir $SUIT_NAME"strassen" ; mv *csv $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
+mkdir $SUIT_NAME"strassen" ; mv *.csv *.energy  $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
 rm -r $SUIT_NAME"strassen" ; sleep 3;
 
 
@@ -52,35 +52,35 @@ SUIT_NAME="4b4l_A7_bots_"
 
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/fib.gcc.omp-tasks-tied -o 0 -n 36
-mkdir $SUIT_NAME"fib"; mv *.csv $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
+mkdir $SUIT_NAME"fib"; mv *.csv *.energy  $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
 rm -r $SUIT_NAME"fib"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/nqueens.gcc.omp-tasks-tied -n 13
-mkdir $SUIT_NAME"nqueens"; mv *.csv $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
+mkdir $SUIT_NAME"nqueens"; mv *.csv *.energy  $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
 rm -r $SUIT_NAME"nqueens"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/health.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/health/medium.input
-mkdir $SUIT_NAME"health"; mv *.csv $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
+mkdir $SUIT_NAME"health"; mv *.csv *.energy  $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
 rm -r $SUIT_NAME"health"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/floorplan.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/floorplan/input.20
-mkdir $SUIT_NAME"floorplan"; mv *.csv $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
+mkdir $SUIT_NAME"floorplan"; mv *.csv *.energy  $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
 rm -r $SUIT_NAME"floorplan"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/fft.gcc.omp-tasks-tied -o 0 -n 10000000
-mkdir $SUIT_NAME"fft" ; mv *.csv $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
+mkdir $SUIT_NAME"fft" ; mv *.csv *.energy  $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
 rm -r $SUIT_NAME"fft" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/sort.gcc.omp-tasks-tied -o 0 -n 100000000
-mkdir $SUIT_NAME"sort"; mv *.csv $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
+mkdir $SUIT_NAME"sort"; mv *.csv *.energy  $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
 rm -r $SUIT_NAME"sort" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/sparselu.gcc.for-omp-tasks-tied -o 0 -n 100 -m 100
-mkdir $SUIT_NAME"sparselu" ; mv *.csv $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
+mkdir $SUIT_NAME"sparselu" ; mv *.csv *.energy  $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
 rm -r $SUIT_NAME"sparselu" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A7 /home/odroid/workloads/bots/bin/strassen.gcc.omp-tasks-tied -o 0 -n 4096
-mkdir $SUIT_NAME"strassen" ; mv *csv $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
+mkdir $SUIT_NAME"strassen" ; mv *.csv *.energy  $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
 rm -r $SUIT_NAME"strassen" ; sleep 3;
 
 
@@ -88,35 +88,35 @@ rm -r $SUIT_NAME"strassen" ; sleep 3;
 SUIT_NAME="4b_bots_"
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/fib.gcc.omp-tasks-tied -o 0 -n 36
-mkdir $SUIT_NAME"fib"; mv *.csv $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
+mkdir $SUIT_NAME"fib"; mv *.csv *.energy  $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
 rm -r $SUIT_NAME"fib"; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/nqueens.gcc.omp-tasks-tied -n 13
-mkdir $SUIT_NAME"nqueens"; mv *.csv $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
+mkdir $SUIT_NAME"nqueens"; mv *.csv *.energy  $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
 rm -r $SUIT_NAME"nqueens"; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/health.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/health/medium.input
-mkdir $SUIT_NAME"health"; mv *.csv $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
+mkdir $SUIT_NAME"health"; mv *.csv *.energy  $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
 rm -r $SUIT_NAME"health"; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/floorplan.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/floorplan/input.20
-mkdir $SUIT_NAME"floorplan"; mv *.csv $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
+mkdir $SUIT_NAME"floorplan"; mv *.csv *.energy  $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
 rm -r $SUIT_NAME"floorplan"; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/fft.gcc.omp-tasks-tied -o 0 -n 10000000
-mkdir $SUIT_NAME"fft" ; mv *.csv $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
+mkdir $SUIT_NAME"fft" ; mv *.csv *.energy  $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
 rm -r $SUIT_NAME"fft" ; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/sort.gcc.omp-tasks-tied -o 0 -n 100000000
-mkdir $SUIT_NAME"sort"; mv *.csv $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
+mkdir $SUIT_NAME"sort"; mv *.csv *.energy  $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
 rm -r $SUIT_NAME"sort" ; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/sparselu.gcc.for-omp-tasks-tied -o 0 -n 100 -m 100
-mkdir $SUIT_NAME"sparselu" ; mv *.csv $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
+mkdir $SUIT_NAME"sparselu" ; mv *.csv *.energy  $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
 rm -r $SUIT_NAME"sparselu" ; sleep 3;
 
 taskset -a -c 4-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/strassen.gcc.omp-tasks-tied -o 0 -n 4096
-mkdir $SUIT_NAME"strassen" ; mv *csv $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
+mkdir $SUIT_NAME"strassen" ; mv *.csv *.energy  $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
 rm -r $SUIT_NAME"strassen" ; sleep 3;
 
 
@@ -124,35 +124,34 @@ rm -r $SUIT_NAME"strassen" ; sleep 3;
 SUIT_NAME="4b4l_A15_bots_"
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/fib.gcc.omp-tasks-tied -o 0 -n 36
-mkdir $SUIT_NAME"fib"; mv *.csv $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
+mkdir $SUIT_NAME"fib"; mv *.csv *.energy  $SUIT_NAME"fib"; tar -cf $SUIT_NAME"fib.tar" $SUIT_NAME"fib";
 rm -r $SUIT_NAME"fib"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/nqueens.gcc.omp-tasks-tied -n 13
-mkdir $SUIT_NAME"nqueens"; mv *.csv $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
+mkdir $SUIT_NAME"nqueens"; mv *.csv *.energy  $SUIT_NAME"nqueens"; tar -cf $SUIT_NAME"nqueens.tar" $SUIT_NAME"nqueens";
 rm -r $SUIT_NAME"nqueens"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/health.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/health/medium.input
-mkdir $SUIT_NAME"health"; mv *.csv $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
+mkdir $SUIT_NAME"health"; mv *.csv *.energy  $SUIT_NAME"health"; tar -cf $SUIT_NAME"health.tar" $SUIT_NAME"health"
 rm -r $SUIT_NAME"health"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/floorplan.gcc.omp-tasks-tied -o 0 -f /home/odroid/workloads/bots/inputs/floorplan/input.20
-mkdir $SUIT_NAME"floorplan"; mv *.csv $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
+mkdir $SUIT_NAME"floorplan"; mv *.csv *.energy  $SUIT_NAME"floorplan"; tar -cf $SUIT_NAME"floorplan.tar" $SUIT_NAME"floorplan"
 rm -r $SUIT_NAME"floorplan"; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/fft.gcc.omp-tasks-tied -o 0 -n 10000000
-mkdir $SUIT_NAME"fft" ; mv *.csv $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
+mkdir $SUIT_NAME"fft" ; mv *.csv *.energy  $SUIT_NAME"fft"; tar -cf $SUIT_NAME"fft.tar" $SUIT_NAME"fft"
 rm -r $SUIT_NAME"fft" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/sort.gcc.omp-tasks-tied -o 0 -n 100000000
-mkdir $SUIT_NAME"sort"; mv *.csv $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
+mkdir $SUIT_NAME"sort"; mv *.csv *.energy  $SUIT_NAME"sort" ; tar -cf $SUIT_NAME"sort.tar" $SUIT_NAME"sort"
 rm -r $SUIT_NAME"sort" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/sparselu.gcc.for-omp-tasks-tied -o 0 -n 100 -m 100
-mkdir $SUIT_NAME"sparselu" ; mv *.csv $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
+mkdir $SUIT_NAME"sparselu" ; mv *.csv *.energy  $SUIT_NAME"sparselu" ; tar -cf $SUIT_NAME"sparselu.tar" $SUIT_NAME"sparselu"
 rm -r $SUIT_NAME"sparselu" ; sleep 3;
 
 taskset -a -c 0-7 ./bin/scheduler_A15 /home/odroid/workloads/bots/bin/strassen.gcc.omp-tasks-tied -o 0 -n 4096
-mkdir $SUIT_NAME"strassen" ; mv *csv $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
+mkdir $SUIT_NAME"strassen" ; mv *.csv *.energy  $SUIT_NAME"strassen" ; tar -cf $SUIT_NAME"strassen.tar" $SUIT_NAME"strassen"
 rm -r $SUIT_NAME"strassen" ; sleep 3;
-
 
