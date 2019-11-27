76b7928681581fa737b7c7e9997519bcf9470963
diegobnh <37424347+diegobnh@users.noreply.github.com>
2019-11-27 10:03:18 -0300

    update

--- scripts/2-predictor/predictor_calc_exec_time_and_power.sh
+++ scripts/2-predictor/predictor_calc_exec_time_and_power.sh
@@ -7,13 +7,29 @@ NUM_EXECUTIONS=10
 rm -f *.dat
 
 #In this function i assumed all outputs is in the same folder. Besides that, i assumed there are two columns: first column is the name and second column is the execution time.
+#In this function i assumed each execution is in different folder. Besides that, each file has four column: 1º app_name, 2º timestamp_start, 3º timestamp_stop, 4º exec_time, 5º num_switch
+#In this function i assumed all outputs is in the same folder. Besides that, i assumed there are two columns: first column is the name and second column is the execution time.
 calculate_exec_time_default_case ()
 {
 for ((k = 0; k< ${#APPS[@]}; k++));
-do   
-   #DEFAULT CASE- 4b4l
-   mean=$(cat default_exp* | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash mean 2 | tr "," "." | awk '{printf "%.2f", $1}')
-   stdev=$(cat default_exp* | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash sstdev 2 | tr "," "."| awk '{printf "%.2f", $1}')
+do 
+   rm -f calculate_times
+   cat */Exec_time_and_power.txt | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | awk '{print $2}' > start_times
+   cat */Exec_time_and_power.txt | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | awk '{print $3}' > end_times
+   paste start_times end_times -d ',' > app_times
+
+   for ((i = 1; i<= 10; i++));
+   do
+        start=$(sed -n "${i}"p app_times | awk -F, '{print $1}')        
+        end=$(sed -n "${i}"p app_times | awk -F, '{print $2}')
+        StartDate=$(date -u -d "$start" +"%s")
+        FinalDate=$(date -u -d "$end" +"%s")
+        seconds=$(date -u -d "0 $FinalDate sec - $StartDate sec" +"%H:%M:%S" | awk -F: '{printf "%.2f", ($1*3600)+($2*60)+$3}')
+        echo $seconds >> calculate_times
+   done
+      
+   mean=$(cat calculate_times | tr "." "," | datamash mean 1 | tr "," "." | awk '{printf "%.2f", $1}')
+   stdev=$(cat calculate_times | tr "." "," | datamash sstdev 1 | tr "," "."| awk '{printf "%.2f", $1}')
    root_squared=$(echo "sqrt ( 10 )" | bc -l)
    aux=$(echo $stdev/$root_squared)
    interval=$(echo $aux | awk '{printf "%.2f\n",$1*1.96}')
@@ -22,12 +38,8 @@ do
    #echo $stdev >> default_stdev
    echo -n $interval"," >> default_exec_time_error.dat
 done
+rm *times
 
-#output is the same as below
-#mean.dat
-#88.53,89.34,31.79,65.66,104.89,23.49,54.33,29.33,54.66,28.53,62.61,51.92,
-#error_bar.dat
-#11.19,3.41,0.22,9.04,0.61,0.61,1.06,1.04,0.47,0.31,0.08,1.61,
 }
 
 
@@ -36,9 +48,10 @@ calculate_power_default_case ()
 #each folder has file with all aplications and its total power
 for ((k = 0; k< ${#APPS[@]}; k++));
 do   
+   
    #DEFAULT CASE- 4b4l
-   mean=$(cat */apps_total_energy | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash mean 2 | tr "," "." | awk '{printf "%.2f", $1}')
-   stdev=$(cat */apps_total_energy | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash sstdev 2 | tr "," "."| awk '{printf "%.2f", $1}')
+   mean=$(cat */Exec_time_and_power.txt | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash mean 4 | tr "," "." | awk '{printf "%.2f", $1}')
+   stdev=$(cat */Exec_time_and_power.txt | grep ${APPS[$k]} | tr "," "\t" | tr "." "," | datamash sstdev 4 | tr "," "."| awk '{printf "%.2f", $1}')
    root_squared=$(echo "sqrt ( 10 )" | bc -l)
    aux=$(echo $stdev/$root_squared)
    interval=$(echo $aux | awk '{printf "%.2f\n",$1*1.96}')
@@ -49,9 +62,6 @@ do
 done
 
 }
-
-#In this function i assumed each execution is in different folder. Besides that, each file has four column: 1º app_name, 2º timestamp_start, 3º timestamp_stop, 4º exec_time, 5º num_switch
-
 calculate_exec_time_dynamic_case()
 {
 for ((k = 0; k< ${#APPS[@]}; k++));
