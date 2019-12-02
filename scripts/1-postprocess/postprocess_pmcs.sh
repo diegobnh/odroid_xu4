dfa70e2f3882ba766af440d46c9fcd081f08ef56
diegobnh <37424347+diegobnh@users.noreply.github.com>
2019-12-02 10:02:48 -0300

    update

--- scripts/1-postprocess/postprocess_pmcs.sh
+++ scripts/1-postprocess/postprocess_pmcs.sh
@@ -23,19 +23,15 @@ if [ $1 = "little" ]; then
 	   
 	   #remove the time column and the columns software counters and utilization
 	   cat temp2 | tr "," " " | awk '{$1=$7=$8=$9=$10=""; print}' > temp3 #Toda vez que você omite uma impressão ele coloca espaços em branco que removem o padrão do arquiv. Por isso precisa usar printf depois
-
-	   #calculates pmcs normalizing by CYCLES
-	   cat temp3 | awk '{printf "%.6f,%.6f,%.6f,%.6f\n", $2/$1, $3/$1, $4/$1, $5/$1}' > $pmcs".post_process"	
+	   
+	   cat temp3 | awk '{printf "%.6f,%.6f,%.6f,%.6f,%.6f\n", $1, $2, $3, $4, $5}' > $pmcs".post_process"	
            sed -i '/-nan/d' $pmcs".post_process"
            sed -i '/nan/d' $pmcs".post_process"  
 
 	   #save the num_lines
 	   cat $pmcs".post_process" | wc -l  > $pmcs".lines"
-
 	   rm temp*
-
 	done
-
               
 	NUM_LINHAS_COMUM=10000000
 	FILES=$(ls *.lines)      
@@ -59,7 +55,15 @@ if [ $1 = "little" ]; then
         #remove coluns in range 34-36 -> last 3 columns are null
         cut -d, -f34-36 --complement $OUTPUT_FILE_NAME > temp
         mv temp $OUTPUT_FILE_NAME
-       
+
+        cat $OUTPUT_FILE_NAME | awk '{printf "%.6f\n", ($1+$5+$11+$16+$21+$26+$31+$36+$41)/9}' > cycles_avg
+        cut -d, -f1,5,11,16,21,26,31,36,41 --complement $OUTPUT_FILE_NAME > temp
+        mv temp $OUTPUT_FILE_NAME
+
+        paste cycles_avg $OUTPUT_FILE_NAME -d "," > temp
+        mv temp $OUTPUT_FILE_NAME
+
+       	
 elif [ $1 = "big" ]; then
 	OUTPUT_FILE_NAME="consolidated-pmc-big.csv"
 
@@ -82,9 +86,8 @@ elif [ $1 = "big" ]; then
 	   
 	   #remove the time column and the columns 9,10,11,12 that means cluster utilization and software counters
 	   cat temp2 | tr "," " " | awk '{$1=$9=$10=$11=$12=""; print}' > temp3
-
-	   #calculates pmcs normalizing by CYCLES
-	   cat temp3 | awk '{printf "%.6f,%.6f,%.6f,%.6f,%.6f,%.6f\n", $2/$1, $3/$1, $4/$1, $5/$1, $6/$1, $7/$1}' > $pmcs".post_process"
+	   
+	   cat temp3 | awk '{printf "%.6f,%.6f,%.6f,%.6f,%.6f,%.6f,%.6f\n", $1, $2, $3, $4, $5, $6, $7}' > $pmcs".post_process"
            #division 0 per 0 result values -nan. I just remove theses lines. Sometimes cycles and others counters are zero
            sed -i '/-nan/d' *.post_process
            sed -i '/nan/d' *.post_process	  
@@ -119,28 +122,39 @@ elif [ $1 = "big" ]; then
         #remove coluns in range 57-60 -> last 4 columns are null
         cut -d, -f57-60 --complement $OUTPUT_FILE_NAME > temp
         mv temp $OUTPUT_FILE_NAME
-                
+        
+
+        cat $OUTPUT_FILE_NAME | awk '{printf "%.6f\n", ($1+$5+$11+$16+$21+$26+$31+$36+$41+$46)/10}' > cycles_avg
+        cut -d, -f1,5,11,16,21,26,31,36,41,46 --complement $OUTPUT_FILE_NAME > temp
+        mv temp $OUTPUT_FILE_NAME
+
+        paste cycles_avg $OUTPUT_FILE_NAME -d "," > temp
+        mv temp $OUTPUT_FILE_NAME
+	
+        
 else
    echo "You need pass as argument cluster name: big or little"
    read -p "Press enter to exit!"
    exit 1
 fi
 
+
 #cada aplicação terá um exec_time.average
 #O datamash na versão do arm faz as operações usando "ponto". Então esse código não vai funcionar no arm.
 cat times.txt | tr "." "," | datamash mean 1 | tr "," "." > exec_time.average
+
 
 #Calculate the average for each feature 
 if [ $1 = "little" ]; then
    #o sed remove a primeira linha que possui o nome de cada coluna
    #sed 1d $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-33 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
-   cat $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-33 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
+   cat $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-34 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
    #paste temp exec_time.average -d "," >  $OUTPUT_FILE_NAME".average"
 
 
 elif [ $1 = "big" ]; then 
    #sed 1d $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-56 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
-   cat $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-56 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
+   cat $OUTPUT_FILE_NAME | tr "," "\t" | tr "." "," | datamash mean 1-57 | tr "," "." | tr "\t" "," > $OUTPUT_FILE_NAME".average"
    #paste temp exec_time.average -d "," >  $OUTPUT_FILE_NAME".average"
 fi
 
